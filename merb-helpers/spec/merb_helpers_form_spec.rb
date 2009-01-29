4234c97a7cd673d64948561e7bb4ec3502f27719
Jacques Crocker <merbjedi@gmail.com>
2009-01-28 21:36:43 -0800

    Adding :checked option for radio_button with specs

--- merb-helpers/spec/merb_helpers_form_spec.rb
+++ merb-helpers/spec/merb_helpers_form_spec.rb
@@ -684,6 +684,16 @@ describe "radio_button" do
     r = @c.render :disabled
     r.should match_tag(:input, :type => "radio", :disabled => "disabled")
   end
+  
+  it "should be checked if :checked => true is passed in" do
+    r = @c.render :checked
+    r.should match_tag(:input, :type => "radio", :checked => "checked")
+  end
+  
+  it "should be unchecked if :checked => false is passed in" do
+    r = @c.render :unchecked
+    r.should_not include("checked=")
+  end
 end
 
 describe "radio_group" do
@@ -729,6 +739,11 @@ describe "radio_group" do
     radio = radio.scan(/<[^>]*>/)
     radio[0].should match_tag(:input, :type => "radio", :value => "foowee", :class => "CLASS radio")
     radio[3].should match_tag(:input, :type => "radio", :value => "baree", :class => "BAREE radio")
+  end
+  
+  it "should allow specifying a checked radio button" do
+    r = @c.render :checked
+    r.should match_tag(:input, :value => "bar", :checked => "checked")
   end
 end
 
